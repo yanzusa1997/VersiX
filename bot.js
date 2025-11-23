512edcd89f00579b378ea52c830b996243ba9dd2
yanzusa1997 <yanzusa1997@gmail.com>
2025-11-23 12:55:11 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,8 +10,8 @@ const seedIDs = [
     // "673e0c942c7bfd708b352447", // uncommon strawberry
     "67dc227a59b878f195998e24", // sweet potato
     "673e0c942c7bfd708b35244d", // Rare strawberry
-    "673e0c942c7bfd708b35245f" // peas
-    // "673e0c942c7bfd708b352441" // common strawberry
+    // "673e0c942c7bfd708b35245f" // peas
+    "673e0c942c7bfd708b352441" // common strawberry
     
 ];
 
